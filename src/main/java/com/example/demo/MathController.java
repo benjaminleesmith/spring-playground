7334f2a9906b752b3de48aff36da1132782bfe58
Benjamin Smith <benjamin.lee.smith@gmail.com>
2018-05-15 15:18:14 -0600

    implemented calculate endpoint

--- src/main/java/com/example/demo/MathController.java
+++ src/main/java/com/example/demo/MathController.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +11,19 @@ public class MathController {
     @GetMapping("/pi")
     public String pi() {
         return "3.141592653589793";
+    }
+
+    @GetMapping("/calculate")
+    public String calculate(@RequestParam(required = false, defaultValue = "add") String operation, @RequestParam int x, @RequestParam int y) {
+        switch(operation) {
+            case "multiply":
+                return new Integer(x*y).toString();
+            case "subtract":
+                return new Integer(x-y).toString();
+            case "divide":
+                return new Integer(x/y).toString();
+            default:
+                return new Integer(x+y).toString();
+        }
     }
 }
--- src/test/java/com/example/demo/MathControllerTest.java
+++ src/test/java/com/example/demo/MathControllerTest.java
@@ -8,7 +8,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -20,10 +19,44 @@ public class MathControllerTest {
     private MockMvc mvc;
 
     @Test
-    public void testPi() throws Exception {
+    public void testPiReturnsPi() throws Exception {
         this.mvc.perform(get("/math/pi").accept(MediaType.TEXT_PLAIN))
                 .andExpect(status().isOk())
                 .andExpect(content().string("3.141592653589793"));
     }
 
+    @Test
+    public void testCalculateAddsTwoNumbers() throws Exception {
+        this.mvc.perform(get("/math/calculate?operation=add&x=4&y=6").accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk())
+                .andExpect(content().string("10"));
+    }
+
+    @Test
+    public void testCalculateMultiplesTwoNumbers() throws Exception {
+        this.mvc.perform(get("/math/calculate?operation=multiply&x=4&y=6").accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk())
+                .andExpect(content().string("24"));
+    }
+
+    @Test
+    public void testCalculateSubtractsTwoNumbers() throws Exception {
+        this.mvc.perform(get("/math/calculate?operation=subtract&x=4&y=6").accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk())
+                .andExpect(content().string("-2"));
+    }
+
+    @Test
+    public void testCalculateDividesTwoNubmers() throws Exception {
+        this.mvc.perform(get("/math/calculate?operation=divide&x=30&y=5").accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk())
+                .andExpect(content().string("6"));
+    }
+
+    @Test
+    public void testCalculateDefaultsToAddition() throws Exception {
+        this.mvc.perform(get("/math/calculate?x=3&y=10").accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk())
+                .andExpect(content().string("13"));
+    }
 }
